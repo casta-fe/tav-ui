6261349525d7d0b8aa572739255fee731a5ef750
i7eo <406132614@qq.com>
2022-05-10 16:08:15 +0800

    fix(example): example

--- examples/src/components/Tree/index.vue
+++ examples/src/components/Tree/index.vue
@@ -1,50 +1,68 @@
 <template>
-  <div>
-    <h2>Tree测试</h2>
-    <TaTree :tree-data="state.treeData" />
-  </div>
+  <ta-tree
+    v-model:expandedKeys="expandedKeys"
+    v-model:selectedKeys="selectedKeys"
+    v-model:checkedKeys="checkedKeys"
+    checkable
+    :tree-data="treeData"
+    :replace-fields="replaceFields"
+  />
 </template>
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
+// import { Tree } from 'ant-design-vue';
+import { defineComponent, ref, watch } from 'vue';
 import { TaTree } from '../../../../dist/index.esm';
 import type { TreeDataItem } from 'ant-design-vue/es/tree/Tree';
 
-console.log(TaTree);
 export default defineComponent({
-  name: 'TreePage',
   components: { TaTree },
   setup() {
-    const state = reactive<{
-      treeData: TreeDataItem[];
-    }>({
-      treeData: [],
+    const expandedKeys = ref<string[]>(['0-0-0', '0-0-1']);
+    const selectedKeys = ref<string[]>(['0-0-0', '0-0-1']);
+    const checkedKeys = ref<string[]>(['0-0-0', '0-0-1']);
+
+    const replaceFields = {
+      children: 'child',
+      title: 'name',
+    };
+
+    const treeData: TreeDataItem[] = [
+      {
+        name: 'parent 1',
+        key: '0-0',
+        child: [
+          {
+            name: '张晨成',
+            key: '0-0-0',
+            disabled: true,
+            child: [
+              { name: 'leaf', key: '0-0-0-0', disableCheckbox: true },
+              { name: 'leaf', key: '0-0-0-1' },
+            ],
+          },
+          {
+            name: 'parent 1-1',
+            key: '0-0-1',
+            child: [{ key: '0-0-1-0', name: 'zcvc' }],
+          },
+        ],
+      },
+    ];
+    watch(expandedKeys, () => {
+      console.log('expandedKeys', expandedKeys);
     });
-    setTimeout(() => {
-      state.treeData = [
-        {
-          title: 'parent 1',
-          key: '0-0',
-          child: [
-            {
-              title: '张晨成',
-              key: '0-0-0',
-              disabled: true,
-              child: [
-                { title: 'leaf', key: '0-0-0-0', disableCheckbox: true },
-                { title: 'leaf', key: '0-0-0-1' },
-              ],
-            },
-            {
-              title: 'parent 1-1',
-              key: '0-0-1',
-              child: [{ key: '0-0-1-0', title: 'zcvc' }],
-            },
-          ],
-        },
-      ];
-    }, 3000);
+    watch(selectedKeys, () => {
+      console.log('selectedKeys', selectedKeys);
+    });
+    watch(checkedKeys, () => {
+      console.log('checkedKeys', checkedKeys);
+    });
     return {
-      state,
+      expandedKeys,
+      selectedKeys,
+      checkedKeys,
+      replaceFields,
+      treeData: ref(treeData),
     };
   },
 });
