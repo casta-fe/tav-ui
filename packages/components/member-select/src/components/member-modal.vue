c497985dc9e6c0e6bc806d9a9710663029f6e201
huyinbo <h404569938@qq.com>
2022-05-13 19:06:39 +0800

    fix(): 成员选择器 迁移完成

---
+++ packages/components/member-select/src/components/member-modal.vue
@@ -0,0 +1,369 @@
+<template>
+  <div class="member-select">
+    <div class="member-box">
+      <Tabs v-model:activeKey="tabActive">
+        <TabPane v-if="!hideOrgTabs" key="0" tab="部门">
+          <template v-if="propsData.multiple">
+            <CheckboxGroup v-model:value="checkboxData">
+              <Tree
+                :tree-data="orgList"
+                block-node
+                :expanded-keys="orgExpandedKeys"
+                :auto-expand-parent="autoExpandParent"
+                :selectable="false"
+                :load-data="getOrgUser"
+                :replace-fields="replaceFields"
+                @expand="onExpand"
+              >
+                <template #title="item">
+                  <template v-if="item.isLeaf">
+                    <Checkbox :value="item.id" :disabled="item.disabled">
+                      <firstLetter :value="item" />{{ item.name }}
+                      <template v-if="item.status === 0"> (已冻结) </template>
+                    </Checkbox>
+                  </template>
+                  <template v-else>
+                    <i v-if="item.type == 1" class="icon-select-company" />
+                    <i v-if="item.type == 2" class="icon-select-org" />
+                    <i v-if="item.type == 3" class="icon-select-group" />
+                    {{ item.name }}
+                  </template>
+                </template>
+              </Tree>
+            </CheckboxGroup>
+          </template>
+          <template v-else>
+            <RadioGroup v-model:value="radioData">
+              <Tree
+                :tree-data="orgList"
+                block-node
+                :expanded-keys="orgExpandedKeys"
+                :auto-expand-parent="autoExpandParent"
+                :selectable="false"
+                :load-data="getOrgUser"
+                :replace-fields="replaceFields"
+                @expand="onExpand"
+              >
+                <template #title="item">
+                  <template v-if="item.isLeaf">
+                    <Radio :value="item.id" :disabled="item.disabled">
+                      <firstLetter :value="item" /> {{ item.name }}
+                      <template v-if="item.status === 0"> (已冻结) </template>
+                    </Radio>
+                  </template>
+                  <template v-else>
+                    <i class="icon-select-org" />
+                    {{ item.name }}
+                  </template>
+                </template>
+              </Tree>
+            </RadioGroup>
+          </template>
+        </TabPane>
+        <TabPane key="1" tab="成员">
+          <div class="user-wrap">
+            <div class="letter-list">
+              <span
+                v-for="item in letterList"
+                :key="item.key"
+                :class="{ on: activeLetter == item.key }"
+                @click="letterClick(item.key)"
+                >{{ item.key }}</span
+              >
+            </div>
+            <div id="userListRef" class="user-list">
+              <template v-if="propsData.multiple">
+                <CheckboxGroup v-model:value="checkboxData">
+                  <ul v-for="item in letterList" :key="item.key" :data-id="item.key">
+                    <li :id="'letter-' + item.key">
+                      <b>{{ item.key }}</b>
+                    </li>
+                    <li v-for="v in item.list" :key="v.id">
+                      <Checkbox :value="v.id" :disabled="v.disabled">
+                        <firstLetter :value="v" />{{ v.name }}
+                        <template v-if="v.status === 0"> (已冻结) </template>
+                      </Checkbox>
+                    </li>
+                  </ul>
+                </CheckboxGroup>
+              </template>
+              <template v-else>
+                <RadioGroup v-model:value="radioData">
+                  <ul v-for="item in letterList" :key="item.key" :data-id="item.key">
+                    <li :id="'letter-' + item.key">
+                      <b>{{ item.key }}</b>
+                    </li>
+                    <li v-for="v in item.list" :key="v.id">
+                      <Radio :value="v.id" :disabled="v.disabled">
+                        <firstLetter :value="v" />{{ v.name }}
+                        <template v-if="v.status === 0"> (已冻结) </template>
+                      </Radio>
+                    </li>
+                  </ul>
+                </RadioGroup>
+              </template>
+            </div>
+          </div>
+        </TabPane>
+      </Tabs>
+    </div>
+    <div v-if="propsData.multiple" class="selected-box">
+      <div class="select-hd">
+        <div class="num">已选中 {{ tagList.length }} 个</div>
+        <div class="ctrl"><TaButton type="primary" @click="clearTag">清空选择</TaButton></div>
+      </div>
+      <div class="select-bd">
+        <span v-for="item in tagList" :key="item.id" class="tag">
+          {{ item.name }}
+          <i class="icon-remove" @click="removeTag(item.id)" />
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, inject, onMounted, reactive, toRefs, watch } from 'vue'
+import { Checkbox, CheckboxGroup, Radio, RadioGroup, TabPane, Tabs, Tree } from 'ant-design-vue'
+import pinyin from 'js-pinyin'
+import { TaButton } from '@tav-ui/components/Button'
+import { useMessage } from '@tav-ui/hooks/web/useMessage'
+import FirstLetter from './first-letter.vue'
+import type { LetterItemList, letterItem } from '../types'
+
+const { createConfirm } = useMessage()
+export default defineComponent({
+  components: {
+    Tree,
+    TaButton,
+    Tabs,
+    TabPane,
+    CheckboxGroup,
+    Checkbox,
+    RadioGroup,
+    Radio,
+    FirstLetter,
+  },
+  props: {
+    selectedData: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  emits: ['change'],
+  setup(props, { emit }) {
+    const propsData = inject('propsData') as any
+    const userList = inject('userList') as any
+    const orgList = inject('orgList') as any
+    console.log(orgList)
+    const state = reactive({
+      replaceFields: {
+        title: 'name',
+        value: 'id',
+        key: 'id',
+      },
+      orgExpandedKeys: [] as any[],
+      autoExpandParent: true,
+      tabActive: '1', //tab切换默认栏
+      checkboxData: [] as any[],
+      radioData: '' as any,
+      letterList: [] as letterItem[], //用户列表的字母顺序表
+      listenScroll: true, //是否监听滚动，点击时候要设置为false
+      activeLetter: '', //当前选中的字母
+    })
+    // 多选时候右侧展示的列表
+    const tagList = computed((): any[] => {
+      return userList.value.filter((item: any) => state.checkboxData.some((v) => v === item.id))
+    })
+    // 是否显示组织的tab,如果传入了options 就不显示org？这个很奇怪 不过先留着
+    const hideOrgTabs = computed(() => {
+      return propsData.value.noOrg || !!propsData.value.options
+    })
+    // 移除已选中的用户
+    const removeTag = (id): void => {
+      const data = state.checkboxData
+      const index = data.findIndex((v) => v == id)
+      data.splice(index, 1)
+    }
+    // 清空已选中的列表
+    const clearTag = (): void => {
+      createConfirm({
+        iconType: 'warning',
+        content: '确定清空选中的用户？',
+        onOk() {
+          state.checkboxData = []
+        },
+      })
+    }
+    // 获取全部用户首字母，并将其分类
+    const userDataRest = () => {
+      const letterObj = {}
+      userList.value.forEach((v: any) => {
+        const chart = pinyin.getCamelChars(v.name)[0]
+        const upperChart = chart.toUpperCase()
+        if (!letterObj[upperChart]) {
+          letterObj[upperChart] = [v]
+        } else {
+          letterObj[upperChart].push(v)
+        }
+      })
+      state.letterList = Object.keys(letterObj)
+        .sort()
+        .map((v) => {
+          return {
+            key: v,
+            list: letterObj[v] as LetterItemList[],
+          }
+        })
+      if (state.letterList.length > 0) {
+        state.activeLetter = state.letterList[0].key
+      }
+    }
+    // 点击字母滚动到对应的位置
+    const letterClick = (key: string) => {
+      state.listenScroll = false
+      const dom = document.getElementById(`letter-${key}`)
+      const userListRef = document.getElementById('userListRef')
+      if (dom && userListRef) {
+        userListRef.scrollTop = dom?.offsetTop - 60 || 0
+        state.activeLetter = key
+        // 延迟运行监听滚动
+        setTimeout(() => {
+          state.listenScroll = true
+        }, 500)
+        // let { start } = useScrollTo({
+        //   el: userListRef,
+        //   to: dom?.offsetTop - 60 || 0,
+        //   duration: 50,
+        //   direction: "scrollTop"
+        // });
+        // start();
+      }
+    }
+
+    // 部门树加载用户
+    const getOrgUser = (treeNode: any) => {
+      return new Promise((resolve) => {
+        const oldData = [...treeNode.dataRef.children]
+        if (oldData.length == 0) {
+          console.log('没数据')
+        }
+        const children = userList.value
+          .filter((v: any) => v.organizationId == treeNode.eventKey)
+          .map((v: any) => {
+            v.isLeaf = true
+            // 忽略列表中的用户需要禁止选中
+            // v.disabled = propsData.value.ignoreUser.includes(v.id);
+            return v
+          })
+        treeNode.dataRef.children = deWeightThree([...oldData, ...children])
+        resolve(null)
+      })
+    }
+    // 数据去重
+    const deWeightThree = (arr) => {
+      const map = new Map()
+      for (const item of arr) {
+        if (!map.has(item.id)) {
+          map.set(item.name, item)
+        }
+      }
+      return [...map.values()]
+    }
+    const openFirstOrg = () => {
+      // 默认打开第一个节点，并获取他下面的用户
+      const firstOrg = orgList.value[0]
+      if (firstOrg) {
+        state.orgExpandedKeys = [firstOrg.id]
+        const children = userList.value
+          .filter((v: any) => v.organizationId == firstOrg.id)
+          .map((v: any) => {
+            v.isLeaf = true
+            return v
+          })
+        firstOrg.children = [...firstOrg.children, ...children]
+      }
+    }
+    // 监听滚动 设置当前选中的字母
+    const listenerUserScroll = (): void => {
+      const userListRef: HTMLElement | null = document.getElementById('userListRef')
+      if (userListRef) {
+        userListRef.addEventListener('scroll', () => {
+          console.log(state.listenScroll)
+          if (state.listenScroll) {
+            const ulList = userListRef?.childNodes[0].childNodes as NodeListOf<ChildNode>
+            if (ulList) {
+              ulList.forEach((v: HTMLElement) => {
+                // if(v.offsetTop>0&&)
+                if (v.nodeName == 'UL') {
+                  if (userListRef) {
+                    const dis = v.offsetTop - userListRef.scrollTop
+                    if (dis > 0 && dis < 100) {
+                      state.activeLetter = v.getAttribute('data-id') || ''
+                    }
+                  }
+                }
+              })
+            }
+          }
+        })
+      }
+    }
+    const onExpand = (keys: string[]) => {
+      state.orgExpandedKeys = keys
+      state.autoExpandParent = false
+    }
+    watch(
+      () => orgList.value,
+      (newData) => {
+        if (newData.length > 0) {
+          openFirstOrg()
+        }
+      }
+    )
+    watch(
+      () => state.checkboxData,
+      (val) => {
+        emit('change', val)
+      }
+    )
+    watch(
+      () => state.radioData,
+      (val) => {
+        emit('change', val)
+      }
+    )
+    onMounted(() => {
+      listenerUserScroll()
+    })
+    const pageInit = (): void => {
+      const data: any[] = props.selectedData
+      openFirstOrg()
+      if (propsData.value.multiple) {
+        state.checkboxData = [...data[0]]
+      } else {
+        if (data.length > 0) {
+          state.radioData = data[0]
+        }
+      }
+      if (propsData.value.type == 'user') {
+        userDataRest()
+      }
+    }
+    pageInit()
+    return {
+      ...toRefs(state),
+      hideOrgTabs,
+      propsData,
+      userList,
+      orgList,
+      tagList,
+      getOrgUser,
+      removeTag,
+      clearTag,
+      onExpand,
+      letterClick,
+    }
+  },
+})
+</script>
---
+++ packages/components/member-select/src/member-select.vue
@@ -0,0 +1,322 @@
+<template>
+  <div class="ta-member-select">
+    <div v-if="!noSelect">
+      <template v-if="type == 'user'">
+        <Select
+          ref="userSelectRef"
+          v-model:value="selectedData[0]"
+          show-search
+          dropdown-class-name="ta-member-option"
+          option-filter-prop="label"
+          :allow-clear="allowClear"
+          :options="userOptions"
+          :max-tag-count="maxTagCount"
+          :max-tag-placeholder="maxTagPlaceholder"
+          :disabled="disabled"
+          :placeholder="placeholder"
+          :mode="multiple ? 'multiple' : undefined"
+          :autofocus="autofocus"
+          :default-open="defaultOpen"
+          :get-popup-container="getPopupContainer"
+          @dropdown-visible-change="userVisibleChange"
+          @change="emitHandle"
+        >
+          <template #option="item">
+            <div class="ta-member-option-item">
+              <span>{{ item.label }} <template v-if="item.status === 0"> (已冻结) </template></span>
+              <span>{{ item.sex == 1 ? '男' : '女' }}</span>
+              <span>{{ item.phone }}</span>
+            </div>
+          </template>
+          <template #dropdownRender="{ menuNode: menu }">
+            <v-nodes :vnodes="menu" />
+            <div
+              v-if="userList.length > 0"
+              class="ta-member-option-more"
+              @mousedown="(e) => e.preventDefault()"
+              @click="showModal"
+            >
+              <a href="javascript:;">查看更多</a>
+            </div>
+          </template>
+        </Select>
+      </template>
+      <template v-else>
+        <TreeSelect
+          v-model:value="selectedData[0]"
+          dropdown-class-name="ta-member-tree"
+          tree-node-filter-prop="title"
+          show-search
+          tree-icon
+          :tree-default-expanded-keys="orgExpandedKey"
+          :allow-clear="allowClear"
+          :max-tag-count="maxTagCount"
+          :max-tag-placeholder="maxTagPlaceholder"
+          :disabled="disabled"
+          :placeholder="placeholder"
+          :multiple="multiple"
+          :tree-data="orgList"
+          :replace-fields="orgFileds"
+          :autofocus="autofocus"
+          :default-open="defaultOpen"
+          :get-popup-container="getPopupContainer"
+          @change="emitHandle"
+        >
+          <!-- :treeDefaultExpandedKeys="orgExpandedKey" -->
+          <!-- 自己循环得递归，暂时不这样写 -->
+          <!-- <TreeSelectNode v-for="item in orgList" :key="item.id">
+          {{item.name}}
+        </TreeSelectNode> -->
+        </TreeSelect>
+      </template>
+    </div>
+    <BasicModal
+      :title="title"
+      :width="850"
+      wrap-class-name="ta-member-modal"
+      :destroy-on-close="true"
+      :get-container="getPopupContainer"
+      @register="registerMemberModal"
+    >
+      <MemberModal :selected-data="selectedData" @change="modalChange" />
+      <template #footer>
+        <Button type="primary" @click="modalSubmit">确定</Button>
+        <Button @click="hideModal">取消</Button>
+      </template>
+    </BasicModal>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, provide, reactive, ref, toRefs, watch } from 'vue'
+import { Select, TreeSelect } from 'ant-design-vue'
+import Button from '@tav-ui/components/button'
+import BasicModal from '@tav-ui/components/modal'
+import { useModal } from '@tav-ui/components/modal/src/hooks/useModal'
+import { memberSelectProps } from './types'
+import MemberModal from './components/member-modal.vue'
+import type { Options } from './types'
+
+export default defineComponent({
+  name: 'TaMemberSelect',
+  components: {
+    VNodes: (_, { attrs }) => {
+      return attrs.vnodes
+    },
+    BasicModal,
+    MemberModal,
+    Button,
+    Select,
+    TreeSelect,
+  },
+  props: memberSelectProps,
+  emits: ['change', 'update:value', 'blur'],
+  setup(props, { emit }) {
+    const userSelectRef = ref<any>(null)
+    const state = reactive({
+      selectedData: [] as any[], //组件里面选中的数据
+      catchData: [] as any[],
+      userList: [] as Options[],
+      orgList: [] as any, //组织树下用的数据
+      orgExpandedKey: [] as any[], //默认展开的数据
+      orgFileds: {
+        children: 'children',
+        title: 'name',
+        key: 'id',
+        value: 'id',
+      },
+    })
+    provide(
+      'propsData',
+      computed(() => props)
+    )
+    provide(
+      'userList',
+      computed(() => state.userList)
+    )
+    provide(
+      'orgList',
+      computed(() => state.orgList)
+    )
+    const userOptions = computed(() => {
+      const list: Options[] = []
+      state.userList.forEach((v) => {
+        const value = v.id || v.value
+        // 非ignoreUser的用户才能选择
+        list.push({
+          label: (v.name as string) || v.label,
+          value,
+          phone: v.phone,
+          status: v.status,
+          disabled: v.disabled as boolean, //已冻结（离职）的不能选
+          sex: v.sex,
+        })
+      })
+      return list
+    })
+    const [registerMemberModal, { openModal: openMemberModal, closeModal: closeMemberModal }] =
+      useModal()
+
+    const showModal = () => {
+      // 如果是用户选择器，打开弹窗时候 也请求下组织列表，可以根据组织选择用户
+      if (props.type == 'user') {
+        getUserList()
+        if (!props.noOrg) {
+          getOrgList()
+        }
+      }
+      if (props.type == 'org') {
+        getOrgList()
+      }
+      if (userSelectRef.value) {
+        userSelectRef.value.blur()
+      }
+      // 某些情况下直接拉起弹窗，那么就需要重置下数据和请求用户列表
+      setBaseData()
+      //  延迟出现，防止互相覆盖
+      setTimeout(() => {
+        openMemberModal()
+      }, 500)
+    }
+    const hideModal = () => {
+      closeMemberModal()
+    }
+    // 获取用户数据
+
+    const getUserList = () => {
+      if (Array.isArray(props.options)) {
+        // 将其处理成 人员的数据格式
+        state.userList = getTrueUserList(props.options)
+      } else {
+        props
+          .userListApi({
+            ...props.userListParams,
+          })
+          .then((res) => {
+            state.userList = getTrueUserList(res.data)
+          })
+      }
+    }
+    // 获取组织数据
+    const getOrgList = (): void => {
+      API__SYSTEM_ORGANIZATION_LIST().then((res) => {
+        state.orgList = res.data
+      })
+    }
+    // 弹窗里面的数据变化
+    const modalChange = (value) => {
+      state.catchData = value
+    }
+    // 弹窗下面的确定事件
+    const modalSubmit = (): void => {
+      const data = state.catchData
+      // 多选第一位为数组，单选第一位为字符串
+      state.selectedData[0] = data
+      emitHandle()
+      closeMemberModal()
+    }
+    const emitHandle = (): void => {
+      emit('update:value', state.selectedData[0])
+      emit('change', state.selectedData[0])
+    }
+    // 将传入的value保存为组件使用的数据
+    const setBaseData = (): void => {
+      if (props.multiple) {
+        if (!Array.isArray(props.value)) {
+          console.warn('多选默认值请传入数组')
+          state.selectedData = [[]]
+        } else {
+          state.selectedData = [[...props.value]]
+        }
+      } else {
+        state.selectedData = props.value ? [props.value] : [null]
+      }
+    }
+    // 这块是用户基础数据，更多选项里面也有用
+    const getTrueUserList = (userList = state.userList) => {
+      const list: Options[] = []
+      userList.forEach((v) => {
+        const value = v.id || v.value
+        // 非ignoreUser的用户才能选择
+        if (!props.ignoreUser.includes(value)) {
+          v.disabled = props.ignoreFrozenUser ? v.status === 0 : false
+          list.push(v)
+        }
+      })
+      return list
+    }
+    const userVisibleChange = () => {
+      // if (v) {
+      // }
+    }
+    const orgVisibleChange = () => {
+      // console.log(v);
+    }
+    watch(
+      () => state.orgList,
+      (newData) => {
+        if (props.multiple) {
+          state.orgExpandedKey =
+            state.selectedData[0].length > 0 ? state.selectedData[0] : [newData[0].id]
+        } else {
+          state.orgExpandedKey = state.selectedData[0] ? state.selectedData : [newData[0].id]
+        }
+      },
+      {
+        deep: true,
+      }
+    )
+    watch(
+      () => props.ignoreUser,
+      () => {
+        getUserList()
+      }
+    )
+    watch(
+      () => props.options,
+      (data) => {
+        if (data) {
+          getUserList()
+        }
+      },
+      {
+        deep: true,
+      }
+    )
+    watch(
+      () => props.value,
+      () => {
+        // 组件绑定的是 selectedData第一位
+        setBaseData()
+      }
+    )
+    // 页面初始化
+    const pageInit = (): void => {
+      setBaseData()
+      if (props.type == 'user') {
+        // 如果默认不显示select就不请求用户
+        if (props.noSelect) {
+          return
+        }
+        getUserList()
+      } else {
+        getOrgList()
+      }
+    }
+    pageInit()
+    return {
+      userSelectRef,
+      ...toRefs(state),
+      userOptions,
+      userVisibleChange,
+      orgVisibleChange,
+      showModal,
+      hideModal,
+      modalChange,
+      modalSubmit,
+      emitHandle,
+      registerMemberModal,
+    }
+  },
+})
+</script>
