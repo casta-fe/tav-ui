03ad28499a255fdbd6b15ca8cb7f4bcece62ab96
huyinbo <h404569938@qq.com>
2022-05-24 17:42:37 +0800

    fix(): 添加formTitle组件

---
+++ packages/components/form/src/components/FormTitle.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: huyb
+ * @Descripttion: Think & Action
+ * @Date: 2021-11-26 16:51:10
+-->
+<template>
+  <div class="ta-form-title">
+    {{ title }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="less" scoped>
+.ta-form-title {
+  margin-top: 5px;
+  font-weight: bold;
+  line-height: 1;
+}
+</style>
