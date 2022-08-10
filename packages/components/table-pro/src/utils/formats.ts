f77ce131b7e8250f816773dba34ac9e78c8bced9
mxs <mxsxs@qq.com>
2022-08-10 18:31:20 +0800

    fix(cmp): table formatter

--- packages/components/table-pro/src/utils/formats.ts
+++ packages/components/table-pro/src/utils/formats.ts
@@ -1,4 +1,11 @@
-import { ProvinceCityRecord, formatNumber, formatToDate, isArray, isString } from '@tav-ui/utils'
+import {
+  ProvinceCityRecord,
+  formatNumber,
+  formatToDate,
+  isArray,
+  isFunction,
+  isString,
+} from '@tav-ui/utils'
 import type { VxeGlobalRendererHandles } from 'vxe-table'
 
 function number({ cellValue }, format = 2) {
@@ -7,7 +14,15 @@ function number({ cellValue }, format = 2) {
 function date({ cellValue }, format = 'YYYY-MM-DD') {
   return formatToDate(cellValue, format)
 }
-function geo({ cellValue, row }, noDistrict = true, joinChar = '-') {
+function geo(
+  { cellValue, row },
+  { hideProvince, hideCity, hideDistrict } = {
+    hideProvince: false,
+    hideCity: false,
+    hideDistrict: true,
+  },
+  joinChar = '-'
+) {
   const res: string[] = []
 
   if (!row) return ''
@@ -22,12 +37,17 @@ function geo({ cellValue, row }, noDistrict = true, joinChar = '-') {
     return ''
   }
 
-  ProvinceCityRecord[province] && res.push(ProvinceCityRecord[province])
+  // 省
+  !hideProvince &&
+    !IS_TWO_LEVEL &&
+    ProvinceCityRecord[province] &&
+    res.push(ProvinceCityRecord[province])
+
   // 直辖市不重复 市: 北京-北京市-东城区 -> 北京市-东城区
-  if (city && !IS_TWO_LEVEL) {
+  if (city && !hideCity) {
     ProvinceCityRecord[city] && res.push(ProvinceCityRecord[city])
   }
-  if (district && !noDistrict) {
+  if (district && !hideDistrict) {
     ProvinceCityRecord[district] && res.push(ProvinceCityRecord[district])
   }
 
@@ -45,6 +65,10 @@ export function useFormats(params: VxeGlobalRendererHandles.RenderEditParams): s
   const formatter = column.params?.formatter
 
   if (formatter) {
+    if (isFunction(formatter)) {
+      return formatter({ cellValue: row[column.field], ...params })
+    }
+
     let formatFn: undefined | ((...args: any[]) => any) = undefined
     let formatterArgs: any[] = []
 
