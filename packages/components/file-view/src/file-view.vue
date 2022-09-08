cdc8db6ad64c8402cbe5f3cf55c030503fa97c33
liangqifei <1454554357@qq.com>
2022-09-08 11:06:34 +0800

    feat: 赠机props打印信息

--- packages/components/file-view/src/file-view.vue
+++ packages/components/file-view/src/file-view.vue
@@ -75,7 +75,7 @@ export default defineComponent({
 
       state.filePath = ''
       state.pageLoading = true
-
+      console.log(props.AppId, 'qqqqq')
       globalConfig.value.TaFileView.previewFile(id, props.AppId)
         .then((res) => {
           state.pageLoading = false
