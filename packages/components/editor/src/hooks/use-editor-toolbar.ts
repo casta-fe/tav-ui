632ac7b5361240eb5bc033232a528fb2847ef0a6
i7eo <admin@i7eo.com>
2024-11-07 00:03:40 +0800

    fix: editor utils

--- packages/components/editor/src/hooks/use-editor-toolbar.ts
+++ packages/components/editor/src/hooks/use-editor-toolbar.ts
@@ -8,7 +8,8 @@ export function useEditorToolbar(options: { mergedProps: ComputedRef<EditorProps
   const toolbarConfig = computed(() => {
     return {
       // toolbar: `undo redo removeformat | formatselect bold italic | lists aligns blockquote hr | image media link charmap emoticons times | restoredraft preview fullscreen code help`
-      toolbar: `undo redo removeformat | formatselect bold italic | lists aligns blockquote hr | table uploadimage uploadfile link | restoredraft fullscreen code`,
+      // toolbar: `undo redo removeformat | formatselect bold italic | lists aligns blockquote hr | table uploadimage uploadfile link | restoredraft fullscreen code`,
+      toolbar: `undo redo removeformat | formatselect bold italic | lists aligns blockquote hr | table uploadimage uploadfile link | fullscreen code`,
       toolbarOptions: {
         /** 工具栏是否固定 */
         toolbar_sticky: true,
